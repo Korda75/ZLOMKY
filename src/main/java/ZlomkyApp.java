3af4c060d44ec71b15a92fffcfe546eb93c44e8e
kubam <smesczsk@seznam.cz>
2025-02-17 11:26:47 +0100

    Arithmetic operation on Zlomek

--- src/main/java/ZlomkyApp.java
+++ src/main/java/ZlomkyApp.java
@@ -10,5 +10,18 @@ public class ZlomkyApp {
 
         System.out.printf("Zlomek a = %s\n", a);
 
+        Zlomek b = new Zlomek(2, 5);
+
+        Zlomek c = a.krat(b);
+        System.out.printf("Zlomek a * b = %s\n", c.zkratit());
+
+        Zlomek d = a.deleno(b);
+        System.out.printf("Zlomek a / b = %s\n", d.zkratit());
+
+        Zlomek e = a.plus(b);
+        System.out.printf("Zlomek a + b = %s\n", e.zkratit());
+
+        Zlomek f = a.minus(b);
+        System.out.printf("Zlomek a - b = %s\n", f.zkratit());
     }
 }
