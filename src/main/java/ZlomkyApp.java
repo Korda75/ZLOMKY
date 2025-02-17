8de016430d59eae7964d62526ddad9b42df958e1
kubam <smesczsk@seznam.cz>
2025-02-17 10:50:00 +0100

    Added Zlomek immutable POJO class

--- src/main/java/ZlomkyApp.java
+++ src/main/java/ZlomkyApp.java
@@ -1,6 +1,14 @@
+import cz.uhk.zlomky.model.Zlomek;
+
 public class ZlomkyApp {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Zlomkoaplikace");
+
+        //vytvoření instance(objektu) třády Zlomku
+        Zlomek a = new Zlomek(3, 4);
+
+        System.out.printf("Zlomek a = %s\n", a);
+
     }
 }
